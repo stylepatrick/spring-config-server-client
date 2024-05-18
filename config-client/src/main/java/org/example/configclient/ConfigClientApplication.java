778919343861281a2bf34c2de4e364de5666e531
stylepatrick <ahrntal.patrick@gmail.com>
2024-05-18 20:21:59 +0200

    Example for Spring config server and client

---
+++ config-client/src/main/java/org/example/configclient/ConfigClientApplication.java
@@ -0,0 +1,24 @@
+package org.example.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    @Value("${foo.bar}")
+    private String fooBar;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @GetMapping(value = "/test")
+    public String test() {
+        return fooBar;
+    }
+}
---
+++ config-server/src/test/java/org/example/springconfigserver/SpringConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
